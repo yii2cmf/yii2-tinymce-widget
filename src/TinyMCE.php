73dec75c74012e175e58dfb1a3a529465bb2dcf8
shandyrov <shandyrov@gmail.com>
2019-08-14 23:30:39 +0500

    Refactoring

--- src/TinyMCE.php
+++ src/TinyMCE.php
@@ -3,7 +3,9 @@ namespace yii2cmf\tinymce;
 
 use Yii;
 use yii\helpers\Html;
+use yii\helpers\Json;
 use yii\helpers\Url;
+use yii\web\JsExpression;
 use yii\web\View;
 use yii\widgets\InputWidget;
 
@@ -19,12 +21,6 @@ class TinyMCE extends InputWidget
     public $defaultPlugins = [
         'code', 'table', 'media', 'image', 'paste', 'imagetools', 'link', 'advlist'
     ];
-
-
-    public function init()
-    {
-        parent::init();
-    }
 
     public function run()
     {
@@ -42,88 +38,34 @@ class TinyMCE extends InputWidget
 
         $options_id = $this->options['id'];
 
-        $lang = $this->getLanguage();
-        $plugins = $this->getPlugins();
-        $external_pluging = $this->getExternalPlugins();
-        $branding = $this->getBoolToString($this->getClientOption('branding', true));
-        $menubar = $this->getBoolToString($this->getClientOption('menubar', true));
-        $toolbar = $this->getBoolToString($this->getClientOption('toolbar', true));
-        $height = $this->getClientOption('height', '400px');
-        $width = $this->getClientOption('width', '100%');
-        $file_picker_types = $this->getClientOption('file_picker_types', 'image');
-        $automatic_uploads = $this->getBoolToString($this->getClientOption('automatic_uploads',false));
-        $external_filemanager_path = $this->getExternalFilemanagerPath();
+        $selector = ['selector' => "#$options_id"];
 
-        $filemanager_title = $this->getClientOption('filemanager_title', 'File Manager');
+        if (!empty($this->clientOptions)) {
 
-        $image_upload_url = $this->getClientOption('image_upload_url','');
-        $images_upload_handler = $this->getImagesUploadHandler();
+            if (empty($this->clientOptions['images_upload_handler']) && isset($this->clientOptions['images_upload_url'])) {
+                $script = new JsExpression($this->getImagesUploadHandler());
+                $this->clientOptions['images_upload_handler'] = $script;
+            }
 
-        $this->getView()->registerJs("
-            tinymce.init({
-              selector: \"#$options_id\",
-              plugins: '$plugins',
-              height: \"$height\",
-              width: \"$width\",
-              branding: $branding,
-              menubar: $menubar,
-              toolbar: $toolbar,
-              language: \"$lang\",
-              language_url: \"$bundle->baseUrl/langs/$lang.js\",
-              images_upload_url: '$image_upload_url',
-              images_upload_handler: $images_upload_handler,
-              automatic_uploads: $automatic_uploads,
-              file_picker_types: '$file_picker_types' ,
-              filemanager_title: '$filemanager_title',
-              external_filemanager_path: '$external_filemanager_path',
-              external_plugins: $external_pluging    
-        });
-        ", View::POS_READY);
+            if (!empty($this->clientOptions['language']) && empty($this->clientOptions['language_url'])) {
+                $lang = $this->getLanguage();
+                $this->clientOptions['language_url'] = "$bundle->baseUrl/langs/$lang.js";
+            }
+
+            $options = Json::encode($selector+$this->clientOptions);
+        } else {
+            $options = Json::encode($selector);
+        }
+        $options = Json::encode(array_merge($selector,$this->clientOptions));
+
+        $js = "tinymce.init($options)";
+
+        $this->getView()->registerJs($js,View::POS_READY);
     }
 
     private function getLanguage()
     {
-        if ($this->language) {
-            return $this->filterLanguage($this->language);
-        }
-        return $this->filterLanguage(\Yii::$app->language);
-    }
-
-    private function getPlugins()
-    {
-        if (isset($this->clientOptions['plugins'])) {
-            return implode(',', $this->clientOptions['plugins']);
-        }
-        return implode(' ',$this->defaultPlugins);
-    }
-
-    private function getExternalPlugins()
-    {
-        if (isset($this->clientOptions['external_plugins'])) {
-            return json_encode($this->clientOptions['external_plugins']);
-        }
-        return '';
-    }
-
-    private function getExternalFilemanagerPath()
-    {
-        if (isset($this->clientOptions['external_filemanager_path'])) {
-            return $this->clientOptions['external_filemanager_path'];
-        }
-        return '';
-    }
-
-    private function getClientOption($option_name, $default)
-    {
-        if (isset($this->clientOptions[$option_name])) {
-            return $this->clientOptions[$option_name];
-        }
-        return $default;
-    }
-
-    private function getBoolToString($val)
-    {
-        return $val ? 'true' : 'false';
+        return strlen(Yii::$app->language) > 2 ? substr(Yii::$app->language,0, strpos(Yii::$app->language,'-')) : Yii::$app->language;
     }
 
     private function filterLanguage($language)
@@ -140,22 +82,23 @@ class TinyMCE extends InputWidget
 
     private function getImagesUploadHandler()
     {
-        $image_upload_url = $this->getClientOption('image_upload_url', '');
-        return " function(blobInfo, success, failure){                
-                var xhr, formData;
-                                                
+        $images_upload_url = $this->clientOptions['images_upload_url'];
+
+        return "function(blobInfo, success, failure){
+            var xhr, formData;
+                                               
                 xhr = new XMLHttpRequest();
                 xhr.withCridentials = false;
-                xhr.open('POST', '$image_upload_url');
+                xhr.open('POST', '$images_upload_url');
                 xhr.onload = function () {
                     var json;
                     if (xhr.status != 200) {
                         failure('HTTP Error: ' + xhr.status);
                         return;
                     }
-                    
+
                     json = JSON.parse(xhr.responseText);
-                    
+
                     if (!json || typeof json.location != 'string') {
                         failure('Invalid JSON: ' + xhr.responseText);
                         return;
